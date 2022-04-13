a26906ffb24b98c58d8d991664c6330edb3f77fe
Jacky Liang <jliang06@uoguelph.ca>
2022-04-13 00:28:07 -0400

    Add input to HTML

--- MegaChatRoom.UI/src/index.ts
+++ MegaChatRoom.UI/src/index.ts
@@ -1,3 +1,9 @@
-export function logToConsole(message: string): void {
-  console.log(message);
+const sendButton = document.getElementById('send-button')!;
+
+sendButton.addEventListener('click', sendMessage);
+
+function sendMessage(): void {
+  const message = document.getElementById('message-input')! as HTMLInputElement;
+
+  console.log(message.value);
 }
