89655f98c9a35788288a9081234a98fe10024435
Jacky Liang <jliang06@uoguelph.ca>
2022-04-25 20:19:36 -0400

    Add SignalR

---
+++ MegaChatRoom.API/MegaChatRoom.API/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MegaChatRoom.API.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(long username, string message)
+        {
+            await Clients.Others.SendAsync("messageReceived", username, message);
+        }
+    }
+}
--- MegaChatRoom.UI/src/index.ts
+++ MegaChatRoom.UI/src/index.ts
@@ -1,9 +1,18 @@
+import * as signalR from '@microsoft/signalr';
+
+const hubConnection = new signalR.HubConnectionBuilder().withUrl('http://localhost:5073/hub').build();
 const sendButton = document.getElementById('send-button')!;
+const username = new Date().getTime();
 
 sendButton.addEventListener('click', sendMessage);
+hubConnection.start().catch((err) => console.log(err));
+
+hubConnection.on('messageReceived', (username: number, message: string) => {
+  console.log(`${username}: ${message}`);
+})
 
 function sendMessage(): void {
   const message = document.getElementById('message-input')! as HTMLInputElement;
 
-  console.log(message.value);
+  hubConnection.send('sendMessage', username, message.value);
 }
